a5d13d2a78937f3c665b6ba4a55d1e4be6b0730c
DescomplicaDevDan <texeirad042@gmail.com>
2026-03-30 20:43:24 -0300

    feat: setup inicial do projeto Nutricomp com Vite e Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-green-600 h-screen flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold p-8 bg-black/20 rounded-lg">
+        TAILWIND ATIVADO! 🍱
+      </h1>
+    </div>
+  )
+}
+
+export default App
